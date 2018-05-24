97d099413dff9e61ced937ae4baf22d313d58b37
Eric <erichech@gmail.com>
2018-05-24 18:15:50 -0400

    defined DATABASES

--- djorg/djorg/settings.py
+++ djorg/djorg/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
