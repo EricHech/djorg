a7651d7c94bdd6d44ab906737d4c712f5a0223c4
Eric <erichech@gmail.com>
2018-06-05 14:35:43 -0400

    untested graphql mutation addition

--- djorg/notes/schema.py
+++ djorg/notes/schema.py
@@ -17,9 +17,10 @@ class Note(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    notes = graphene.List(Note)
+    note = graphene.Field(Note, id=graphene.String(), title=graphene.String())
+    all_notes = graphene.List(Note)
 
-    def resolve_notes(self, info):
+    def resolve_all_notes(self, info):
         """Decide which notes to return."""
 
         user = info.context.user  # Use docs or debugger to find this
@@ -30,6 +31,46 @@ class Query(graphene.ObjectType):
             return NoteModel.objects.none()
         else:
             return NoteModel.objects.filter(user=user)
+
+    def resolve_note(self, info, **kwargs):
+        title = kwargs.get("title")
+        id = kwargs.get('id')
+
+        # maybe add a filter before the .get to only search the right user
+        if title is not None:
+            return NoteModel.objects.get(title=title)
+
+        return None
+
+
+class CreateNote(graphene.Mutation):
+
+    class Arguments:
+        # Input attributes for the mutation
+        title = graphene.String()
+        content = graphene.String()
+
+    # Output fields after mutation
+    ok = graphene.Boolean()
+    note = graphene.Field(Note)
+
+    def mutate(self, info, title, content):
+        user = info.context.user
+
+        if user.is_anonymous:
+            is_ok = False
+            return CreateNote(ok=new_ok)
+
+        else:
+            new_note = NoteModel(title=title, content=content, user=user)
+            new_note.save()
+            is_ok = True
+
+            return CreateNote(note=new_note, ok=is_ok)
+
+
+class Mutation(graphene.ObjectType):
+    create_note = CreateNote.Field()
 
 # Add a schema and attach the query
 schema = graphene.Schema(query=Query)
